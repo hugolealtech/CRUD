638f1a5e0a231aa3bd29e6b2f0d7212040ddd285
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2025-02-01 10:13:56 -0300

    corrigido Toggle

--- codeComparing/task-item.component.ts
+++ codeComparing/task-item.component.ts
@@ -1,21 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Tarefa } from '../../models/Tarefas';
+import { Tarefa } from '../../models/Tarefa';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-@Component({
-  selector: 'app-tasks-item',
-  standalone:true,
-  imports: [FontAwesomeModule, CommonModule],
-  templateUrl: './tasks-item.component.html',
-  styleUrl: './tasks-item.component.css'
-})
-export class TasksItemComponent {
 
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
   @Input() tarefa!:Tarefa;
-  @Output() onDeleteTask = new EventEmitter<Tarefa>();
+  @Output() onDeleteTask = new EventEmitter<Tarefa>();  // envia a tarefa deletada
   @Output() onToggleConcluido = new EventEmitter<Tarefa>();
 
 
@@ -28,5 +28,4 @@ export class TasksItemComponent {
   onToggle(tarefa:Tarefa){
     this.onToggleConcluido.emit(tarefa);
   }
-
 }
